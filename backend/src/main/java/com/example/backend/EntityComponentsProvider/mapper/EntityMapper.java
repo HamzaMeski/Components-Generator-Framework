2ca4164758870757b991acf710001c33823df45d
HamzaMeski <meskihamza5@gmail.com>
2024-12-01 12:30:37 +0100

    fixing issues

--- backend/src/main/java/com/example/backend/EntityComponentsProvider/mapper/EntityMapper.java
+++ backend/src/main/java/com/example/backend/EntityComponentsProvider/mapper/EntityMapper.java
@@ -3,14 +3,10 @@ package com.example.backend.EntityComponentsProvider.mapper;
 import com.example.backend.EntityComponentsProvider.dto.request.CreateDTO;
 import com.example.backend.EntityComponentsProvider.dto.request.UpdateDTO;
 import com.example.backend.EntityComponentsProvider.dto.response.ResponseDTO;
-import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
 public interface EntityMapper<T, ID> {
     T toEntity(CreateDTO<T> request);
-
     ResponseDTO<T, ID> toResponseDTO(T entity);
-
     void updateEntity(UpdateDTO<T> request, @MappingTarget T entity);
 }
