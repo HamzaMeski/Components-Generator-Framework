3624cfd7818e56f43d66a052cf15106b33b80a49
HamzaMeski <meskihamza5@gmail.com>
2024-12-07 17:18:36 +0100

    Implementing arch setup

--- backend/src/main/java/com/example/backend/components/trainer/repository/TrainerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.components.trainer.repository;
-
-import com.example.backend.EntityComponentsProvider.repository.EntityRepository;
-import com.example.backend.entities.Trainer;
-
-public interface TrainerRepository extends EntityRepository<Trainer, Long> {
-}
